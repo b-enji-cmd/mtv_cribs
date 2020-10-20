0526a1d3c2cbfae7a1c26e7c39e0755cd15ee43b
Ben Fulton <benjamin.fulton99@gmail.com>
2020-10-20 10:51:53 -0600

    Add almost all working test cases for Iteration 3

--- lib/building.rb
+++ lib/building.rb
@@ -7,6 +7,7 @@ class Building
     def add_unit(unit)
         @units << unit
     end
+
     def renters
         unit_renters = @units.map do |unit|
             unit.renter
@@ -19,5 +20,25 @@ class Building
         total_rent += unit.monthly_rent
        end
        average_rent = total_rent / @units.count
+    end
+
+    def rented_units
+        units_rented = @units.map do |unit|
+            if unit.renter != nil
+                unit
+            end
+        end
+        #There has to be a better way to do this, this feels clunky
+    end
+
+    def renter_with_highest_rent
+        @units.max_by{|unit| unit.monthly_rent}.renter
+    end
+
+    def units_by_number_of_bedrooms
+        @units.group_by {|rooms| rooms.bedrooms}
+        
+        #unit_by_room is a hash that does what is asked, just not in the right format :/
+        #not sure how to only get the number to return
     end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -9,8 +9,13 @@ class ApartmentTest < Minitest::Test
         @building = Building.new
         @renter1 = Renter.new("Aurora")
         @renter2 = Renter.new("Tim")
+        @renter3 = Renter.new("Spencer")
+        @renter4 = Renter.new("Jessie")
+        @renter5 = Renter.new("Max")
         @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
         @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+        @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+        @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
     end
     def test_it_exists
         assert_instance_of Building , @building
@@ -44,5 +49,30 @@ class ApartmentTest < Minitest::Test
         @unit2.add_renter(@renter2)
        
         assert_equal 1099.5 , @building.average_rent
+    end
+    def test_it_can_return_rented_units
+        @building.add_unit(@unit2)
+        @unit2.add_renter(@renter2)
+        assert_equal [@unit2], @building.rented_units
+    end
+
+    def test_it_can_give_renter_with_highest_rent
+        @building.add_unit(@unit1)
+        @building.add_unit(@unit2)
+        @building.add_unit(@unit3)
+        @unit2.add_renter(@renter3)
+        @unit1.add_renter(@renter4)
+        
+        assert_equal @renter4 , @building.renter_with_highest_rent
+    end
+
+    def test_it_can_return_units_by_rooms
+        @building.add_unit(@unit1)
+        @building.add_unit(@unit2)
+        @building.add_unit(@unit3)
+        @building.add_unit(@unit4)
+
+    
+        binding.pry
     end
 end
